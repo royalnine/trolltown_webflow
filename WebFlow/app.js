9135e17305b11d97f72e4bc1d9410e6b9d9cd99c
Aidar P <17271567+royalnine@users.noreply.github.com>
2022-06-18 19:39:15 +0100

    adding shit

--- WebFlow/app.js
+++ WebFlow/app.js
@@ -99,7 +99,7 @@ const connectWallet = async () => {
 const mintTrolls = async (web3, numberOfTokens, contractAddress) => {
   var contractABI;
   await $.getJSON(
-    "https://api.jsonbin.io/b/62a10f41449a1f3821019f72", //change this 
+    "https://api.jsonbin.io/b/62ae1b85449a1f38210f11c4", //change this 
     function (data) {
         // JSON result in `data` variable
         console.log("Got contract abi");
